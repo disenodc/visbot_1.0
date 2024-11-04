4b47277ed2f69b7a658c3195881986c2617f6dba
disenodc <disenodc@gmail.com>
2024-11-04 13:49:10 -0300

    con openai api

--- visbot.py
+++ visbot.py
@@ -5,6 +5,7 @@ import requests
 import openai
 
 # Configura tu clave API de OpenAI
+openai.api_key = 'sk-proj-nsJb8ICD7OEXNdvuUMsgT3BlbkFJjQLKNUnCid5UgFAQmwHO'
 
 st.set_page_config(
     page_title="VisBot - Visualization Recommender with AI",
